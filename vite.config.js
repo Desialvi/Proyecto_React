24507cdb4ef4a84f48b701488dba3b9db59487fe
Desialvi <desialvi@gmail.com>
2025-10-24 02:20:03 -0300

    actualizacion

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/proyecto_react_v1/'  
+  base: '/Proyecto_React/'  
 });
