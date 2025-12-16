a8595a49c12d87b2f059b7592e1f82df2838ab83
Desialvi <desialvi@gmail.com>
2025-12-16 00:05:22 -0300

    arreglando

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <main className="admin">
+      <h2>Panel de Administración</h2>
+      <p>Contenido protegido para usuarios autenticados</p>
+    </main>
+  );
+}
